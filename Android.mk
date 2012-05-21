2d0c9fed3e282c9215965097d96ffa81e42bbd69
Jan Berkel <jan.berkel@gmail.com>
2012-05-21 20:23:53 +0200

    Android conventions

--- Android.mk
+++ Android.mk
@@ -3,7 +3,7 @@ LOCAL_PATH := $(call my-dir)
 # Build liblimiter~.so
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := limiter~
+LOCAL_MODULE := limiter_tilde
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../libpd/pure-data/src
 LOCAL_CFLAGS := -DPD
 LOCAL_SRC_FILES := src/limiter~.c
@@ -14,7 +14,7 @@ include $(BUILD_SHARED_LIBRARY)
 # Build libz~.so
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := z~
+LOCAL_MODULE := z_tilde
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../libpd/pure-data/src
 LOCAL_CFLAGS := -DPD
 LOCAL_SRC_FILES := src/z~.c
